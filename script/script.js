ecebf3f7327f7244a4352906c44ef2c3a986be0a
Breno Almeida <brenimsilva7@gmail.com>
2022-02-26 03:05:03 -0300

    v0.2.3
    -Now you can increase any stats, and the attributes will change accordingly
    -Changed icon colors

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ function heroes(
   classHero,
   maxHP,
   HP,
+  maxSP,
   SP,
   STR,
   DEX,
@@ -20,6 +21,7 @@ function heroes(
   this.classHero = classHero;
   this.maxHP = maxHP;
   this.HP = HP;
+  this.maxSP = maxSP;
   this.SP = SP;
   this.STR = STR;
   this.DEX = DEX;
@@ -38,7 +40,7 @@ function heroes(
   this.maxAtk = 0;
 }
 //Set Warrio Class Values
-const warriorStats = ["Heroe", "Warrior", 50, 50, 0, 3, 2, 1, 2, 1, 0, 0, 1];
+const warriorStats = ["Heroe", "Warrior", 50, 50, 0, 0, 3, 2, 1, 2, 1, 0, 0, 1];
 const warrior = new heroes();
 let index = 0;
 for (let element in warrior) {
@@ -58,12 +60,12 @@ function defineAtributes(heroe) {
   const maxAtk = Math.round(heroe.STR * 1.1);
   heroe.minAtk = minAtk;
   heroe.maxAtk = maxAtk;
-  heroe.maxHP = Math.round(heroe.STR * 1.2) + heroe.VIT * 5 + 50;
+  heroe.maxHP = Math.round(heroe.STR * 1.2) + heroe.VIT * 5 + warriorStats[2];
   heroe.ACC = heroe.DEX * 5;
   heroe.DEF = heroe.DEX * 2 + heroe.VIT * 2;
   heroe.MAG = heroe.INT * 4;
   heroe.SPD = heroe.AGI * 3;
-  heroe.SP = heroe.INT * 5;
+  heroe.maxSP = heroe.INT * 5;
 }
 
 //Stats Initialization
@@ -75,9 +77,10 @@ document.getElementById("INT").textContent = warrior.INT;
 defineAtributes(warrior);
 
 //Atribute initialization
-document.getElementById("HP").textContent = warrior.HP;
-document.getElementById("HP").textContent = warrior.maxHP;
-document.getElementById("SP").textContent = warrior.SP;
+warrior.HP = warrior.maxHP;
+warrior.SP = warrior.maxSP;
+document.getElementById("HP").textContent = `${warrior.HP}/${warrior.maxHP}`;
+document.getElementById("SP").textContent = `${warrior.SP}/${warrior.maxSP}`;
 document.getElementById(
   "ATK"
 ).textContent = `${warrior.minAtk} - ${warrior.maxAtk}`;
@@ -85,6 +88,8 @@ document.getElementById("MAG").textContent = warrior.MAG;
 document.getElementById("DEF").textContent = warrior.DEF;
 document.getElementById("ACC").textContent = warrior.ACC;
 document.getElementById("SPD").textContent = warrior.SPD;
+
+// -------------------------Upgrade Stats------------------------- //
 
 //Buton STR
 document.querySelector(".arrow-str").addEventListener("click", function () {
@@ -94,9 +99,46 @@ document.querySelector(".arrow-str").addEventListener("click", function () {
   document.getElementById(
     "ATK"
   ).textContent = `${warrior.minAtk} - ${warrior.maxAtk}`;
-  document.getElementById("HP").textContent = warrior.maxHP;
+  document.getElementById("HP").textContent = `${warrior.HP}/${warrior.maxHP}`;
   percentPlayer = definePercentages(warrior.maxHP, warrior.HP);
   updateProgressBarPlayer(playerBar, warrior.HP, warrior.maxHP, percentPlayer);
+});
+
+//Buton DEX
+document.querySelector(".arrow-dex").addEventListener("click", function () {
+  warrior.DEX++;
+  defineAtributes(warrior);
+  document.getElementById("DEX").textContent = warrior.DEX;
+  document.getElementById("ACC").textContent = warrior.ACC;
+  document.getElementById("DEF").textContent = warrior.DEF;
+});
+
+//Buton AGI
+document.querySelector(".arrow-agi").addEventListener("click", function () {
+  warrior.AGI++;
+  defineAtributes(warrior);
+  document.getElementById("AGI").textContent = warrior.AGI;
+  document.getElementById("SPD").textContent = warrior.SPD;
+});
+
+//Buton VIT
+document.querySelector(".arrow-vit").addEventListener("click", function () {
+  warrior.VIT++;
+  defineAtributes(warrior);
+  document.getElementById("VIT").textContent = warrior.VIT;
+  document.getElementById("HP").textContent = `${warrior.HP}/${warrior.maxHP}`;
+  document.getElementById("DEF").textContent = warrior.DEF;
+  percentPlayer = definePercentages(warrior.maxHP, warrior.HP);
+  updateProgressBarPlayer(playerBar, warrior.HP, warrior.maxHP, percentPlayer);
+});
+
+//Buton INT
+document.querySelector(".arrow-int").addEventListener("click", function () {
+  warrior.INT++;
+  defineAtributes(warrior);
+  document.getElementById("INT").textContent = warrior.INT;
+  document.getElementById("SP").textContent = `${warrior.SP}/${warrior.maxSP}`;
+  document.getElementById("MAG").textContent = warrior.MAG;
 });
 
 // -------------------------LVL Sistem------------------------- //
@@ -142,7 +184,7 @@ let percentPlayer = definePercentages(warrior.maxHP, warrior.HP);
 console.log(percentPlayer);
 updateProgressBarPlayer(playerBar, warrior.HP, warrior.maxHP, percentPlayer);
 
-//ATACAR MOB
+// -------------------------Attacks------------------------- //
 document.querySelector("#sword").addEventListener("click", function () {
   //
   Javali.HP = Javali.HP - damageDealt(warrior.maxAtk, warrior.minAtk);
